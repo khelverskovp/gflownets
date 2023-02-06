434de1f9ffa7296081a9048af250038a83e787b8
marcusroberto02 <marcusrobertonielsen02@gmail.com>
2023-02-06 13:01:21 +0100

    changed reqs

--- toymodels/src/data/make_dataset.py
+++ toymodels/src/data/make_dataset.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import click
 import logging
+import pandas as pd
 from pathlib import Path
 from dotenv import find_dotenv, load_dotenv
 
@@ -14,6 +15,8 @@ def main(input_filepath, output_filepath):
     """
     logger = logging.getLogger(__name__)
     logger.info('making final data set from raw data')
+
+
 
 
 if __name__ == '__main__':
