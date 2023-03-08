43084c16f4eaccaccbcce232bf622c97bb893c0c
marcusroberto02 <marcusrobertonielsen02@gmail.com>
2023-03-08 17:15:48 +0100

    added plot code for all blocks with default stems plot

--- src/visualization/visualizeBlock.py
+++ src/visualization/visualizeBlock.py
@@ -4,7 +4,8 @@ from rdkit.Chem import AllChem, Draw
 import pandas as pd
 import numpy as np
 import os
-
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
 
 class VisualizerBlock:
     # blocks path
@@ -21,7 +22,7 @@ class VisualizerBlock:
         self.block_smis = blocks["block_smi"].to_list()
         self.block_rs = blocks["block_r"].to_list()
 
-    def visualizeBlock(self,idx,show_stems=False):
+    def visualizeBlock(self,idx,figsize=(300,300),show_stems=False):
         # get smiles representation
         smi = self.block_smis[idx]
         # retrieve chemical structure
@@ -34,21 +35,55 @@ class VisualizerBlock:
         os.makedirs(path,exist_ok=True)
         filename = f'block{idx}.png'
         path = path + filename
-        stems = self.block_rs[idx] if show_stems else []
-        Draw.MolToFile(m,path,highlightAtoms=stems)
+        stems = [self.block_rs[idx][0]] if show_stems else []
+        Draw.MolToFile(m,path,size=figsize,highlightAtoms=stems)
+        # write log
+        print("Visualized block:")
+        print(f"SMILES: {smi}")
+        print(f"Path: {path}\n")
+    
+    def visualizeBlockWithDefaultStem(self,idx,figsize=(300,300)):
+        # get smiles representation
+        smi = self.block_smis[idx]
+        # retrieve chemical structure
+        m = Chem.MolFromSmiles(smi)
+        # compute 2D structure
+        _ = AllChem.Compute2DCoords(m)
+        # draw to file
+        subdir = "default_stems/"
+        path = f'{self.out_dir}{subdir}'
+        os.makedirs(path,exist_ok=True)
+        filename = f'block{idx}.png'
+        path = path + filename
+        stems = [self.block_rs[idx][0]]
+        Draw.MolToFile(m,path,size=figsize,highlightAtoms=stems)
         # write log
         print("Visualized block:")
         print(f"SMILES: {smi}")
         print(f"Path: {path}\n")
 
-if __name__ == "__main__":
-    visualizer = VisualizerBlock()  
+    def getAllBlocksWithDefaultStemPlot(self):
+        fig, ax = plt.subplots(nrows=7,ncols=15,figsize=(15,7))
 
-    for idx in [0,4,10]:
-        m = Chem.MolFromSmiles(visualizer.block_smis[idx])
-        print(f"Block {idx} has {m.GetNumAtoms()} atoms")
-        print(visualizer.block_smis[idx])
-        for atom in m.GetAtoms():
-            print(atom.GetAtomicNum())
+        for idx in range(len(self.block_smis)):
+            self.visualizeBlockWithDefaultStem(idx,figsize=(300,150))
+
+            path = f'{self.out_dir}stems/block{idx}.png'
+            img = mpimg.imread(path)
+        
+            ax[idx // 15, idx % 15].imshow(img)
+            ax[idx // 15, idx % 15].axis('off')
+        
+        fig.suptitle("SMILES block visualization with default stems",fontweight='bold')
+        plt.savefig("reports/figures/allstems.png")
+
+
+if __name__ == "__main__":
+    visualizer = VisualizerBlock() 
+
+    visualizer.getAllBlocksWithDefaultStemPlot()
+
+    
+
 
 
