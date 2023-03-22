bb4e4c0ec79f973ca9f9c85289c3d6bf05e11ed5
marcusroberto02 <marcusrobertonielsen02@gmail.com>
2023-03-22 17:08:05 +0100

    finally made initial paper proxy work

--- src/utils/chem.py
+++ src/utils/chem.py
@@ -4,6 +4,16 @@
 import numpy as np
 from rdkit import Chem
 
+import torch
+from torch_geometric.data import Data
+from torch_sparse import coalesce
+
+
+atomic_numbers = {"H": 1, "He": 2, "Li": 3, "Be": 4, "B": 5, "C": 6, "N": 7, "O": 8, "F": 9, "Ne": 10, "Na": 11, "Mg": 12, "Al": 13, "Si": 14,
+                  "P": 15, "S": 16, "Cl": 17, "Ar": 18, "K": 19, "Ca": 20, "Sc": 21, "Ti": 22, "V": 23, "Cr": 24, "Mn": 25, "Fe": 26, "Co": 27,
+                  "Ni": 28, "Cu": 29, "Zn": 30, "Ga": 31, "Ge": 32, "As": 33, "Se": 34, "Br": 35, "Kr": 36, "Rb": 37, "Sr": 38, "Y": 39,
+                  "Zr": 40, "Nb": 41, "Mo": 42, "Tc": 43, "Ru": 44, "Rh": 45, "Pd": 46, "Ag": 47, "Cd": 48, "In": 49, "Sn": 50, "Sb": 51,
+                  "Te": 52, "I": 53, "Xe": 54, "Cs": 55, "Ba": 56}
 
 def mol_from_jbonds_and_blocks(jbonds, blocks=None, block_smis=None):
     """
@@ -72,3 +82,74 @@ def mol_from_jbonds_and_blocks(jbonds, blocks=None, block_smis=None):
     Chem.SanitizeMol(mol)
     
     return mol, mol_bonds
+
+# taken directly from https://github.com/GFNOrg/gflownet/blob/master/mols/utils/chem.py
+def onehot(arr, num_classes, dtype=int):
+    """
+        params:
+        arr: class indices
+        num_classes: number of classes
+        returns onehot encoded vector of shape (len(arr),num_classes)
+
+        e.g.
+        onehot([0,0,1,2],3) = [[1,0,0],[1,0,0],[0,1,0],[0,0,1]]
+    """
+    arr = np.asarray(arr, dtype=int)
+    assert len(arr.shape) == 1, "dims other than 1 not implemented"
+    onehot_arr = np.zeros(arr.shape + (num_classes,), dtype=dtype)
+    onehot_arr[np.arange(arr.shape[0]), arr] = 1
+    return onehot_arr
+
+
+# inspired heavily by code from https://github.com/GFNOrg/gflownet/blob/master/mols/utils/chem.py
+def mol_to_graph_backend(atmfeat, bond, bondfeat):
+    """"
+    params:
+    atmfeat: atom features (array of shape (natoms, 70))
+    bond: list of bonds (array of shape (nbonds,2))
+    bondfeat: list of bond types for each bond in onehot encoded format (array of shape (nbonds,4))
+
+    convert to PyTorch geometric module graph
+    """
+    # number of atoms in molecule
+    natm = atmfeat.shape[0]
+    # transform to torch_geometric bond format
+    atmfeat = torch.tensor(atmfeat, dtype=torch.float32)
+    # check if any bonds exist
+    if bond.shape[0] > 0:
+        # bond.T will have shape (2, nbonds)
+        # e.g. the first bond goes from bond.T[0][0] to bond.T[1][0]
+        # np.flipud(bond.T) simply flips the matrix upside down
+        # e.g. bond = [[0,1],
+        #              [1,2],
+        #              [2,3]]
+        # bond.T = [[0,1,2],
+        #           [1,2,3]]
+        # np.flipud(bond.T) = [[1,2,3],
+        #                      [0,1,2]]
+        # np.concatenate([bond.T, np.flipud(bond.T)], axis=1)=
+        # [[0,1,2,1,2,3],
+        #  [1,2,3,0,1,2]]
+        # ensures that edges go both ways
+        edge_index = torch.tensor(np.concatenate([bond.T, np.flipud(bond.T)], axis=1), dtype=torch.int64)
+        edge_attr = torch.tensor(np.concatenate([bondfeat, bondfeat], axis=0), dtype=torch.float32)
+        # sorts the bonds afer indices
+        edge_index, edge_attr = coalesce(edge_index, edge_attr, natm, natm)
+        # referring to the same example as before
+        # [[0,1,2,1,2,3],
+        #  [1,2,3,0,1,2]]
+        # after coalesce
+        # [[0,1,1,2,2,3],
+        #  [1,0,2,1,3,2]]
+    else:
+        # no edges occur in the graph
+        edge_index = torch.zeros((0, 2), dtype=torch.int64)
+        edge_attr = torch.tensor(bondfeat, dtype=torch.float32)
+
+    # turn into graph object
+    data = Data(x=atmfeat, edge_index=edge_index, edge_attr=edge_attr)
+    return data
+
+
+if __name__ == "__main__":
+    print(onehot([0,0,0,0,0,1,1,1,1,2,2,2,2,3], 4))
--- src/utils/proxy.py
+++ src/utils/proxy.py
@@ -8,92 +8,246 @@ import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from torch_geometric.nn import NNConv, Set2Set
+from torch_geometric.nn import NNConv
+from torch_geometric.nn.aggr import Set2Set
 from torch_geometric.data import Data, Batch
 import torch_geometric.nn as gnn
+import requests
 
 # import MoleculeMDP from mols.py
-from mols import MoleculeMDP
+from mols import BlockMolecule, MoleculeMDP
+
+from chem import atomic_numbers
+
+from torch_sparse import coalesce
+
+from rdkit import RDConfig
+from rdkit.Chem import ChemicalFeatures
+from rdkit.Chem.rdchem import BondType as BT
+from rdkit.Chem.rdchem import HybridizationType
+
+import os
+
+import pandas as pd
+
+import json
 
 class MPNNet_Atom(nn.Module):
-    def __init__(self, num_feat, dim,
-                 num_out_per_mol,
-                 num_conv_steps, dropout_rate):
+    def __init__(self, num_feat, dim, num_conv_steps, num_out_per_mol=1, dropout_rate=0):
         super().__init__()
-        self.lin0 = nn.Linear(num_feat, dim)
-        self.num_opm = num_out_per_mol
+
+        """
+        params:
+        num_feat: number of input features
+        dim: number of dimension in each hidden layers
+        num_conv_steps: number of convolutional steps
+        num_out_per_mol: number of output features
+        dropout_rate: dropout rate
+        """
+        # 14 + use_stem_mask (always true) + 56
+        # REMEMBER TO COMMENT THIS BETTER!!
+        # num_feat = 71
+
+        # since we are only interested in a single scalar reward
+        # num_out_per_mol = 1, when used to evaluate the proxy
+        self.num_out_per_mol = num_out_per_mol
         self.num_conv_steps = num_conv_steps
         self.dropout_rate = dropout_rate
 
+        # activation function
         self.act = nn.LeakyReLU()
 
+        # first layer - simple layer from num_feat to dim dimensions
+        self.lin0 = nn.Linear(num_feat, dim)
+
+        # defines graph convolutional layer
         net = nn.Sequential(nn.Linear(4, 128), self.act, nn.Linear(128, dim * dim))
         self.conv = NNConv(dim, dim, net, aggr='mean')
 
+        # defines structure for GRU layer
         self.gru = nn.GRU(dim, dim)
         
-
+        # set2set aggregation
         self.set2set = Set2Set(dim, processing_steps=3)
 
-        self.lin = nn.Linear(dim * 2, num_out_per_mol)
+        # last linear linear - ensures that one output is given for each molecule in the batch
+        self.lin1 = nn.Linear(dim * 2, self.num_out_per_mol)
 
-    def forward(self, graph, do_dropout=False):
-        out = self.act(self.lin0(graph.x))
-        h = out.unsqueeze(0)
+        
+
+    def forward(self, data, do_dropout=False):
+        # natomsbatch = number of atoms in total for all molecules in batch
+        # data.x has shape (natomsbatch, num_feat)
+        out = self.act(self.lin0(data.x)) # out has shape (natomsbatch, dim)
+        h = out.unsqueeze(0) # h has shape (1, natomsbatch, dim)
         h = F.dropout(h, training=do_dropout, p=self.dropout_rate)
-
+        
         for i in range(self.num_conv_steps):
-            m = self.act(self.conv(out, graph.edge_index, graph.edge_attr))
+            # num_edges: number of edges/bonds in batch
+            # num_bond_features: number of bond types (4, because single, double, triple and aromatic)
+            # data.edge_index has shape (2, num_edges)
+            # data.edge_attr has shape (num_edges, num_bond_features)
+            m = self.act(self.conv(out, data.edge_index, data.edge_attr)) # m has shape (natomsbatch, dim)
             m = F.dropout(m, training=do_dropout, p=self.dropout_rate)
             out, h = self.gru(m.unsqueeze(0).contiguous(), h.contiguous())
+            # after the gru layer:
+            # out has shape (1, natomsbatch, dim)
+            # h has shape (1, natomsbatch, dim)
             h = F.dropout(h, training=do_dropout, p=self.dropout_rate)
-            out = out.squeeze(0)
+            out = out.squeeze(0) # out has shape (natomsbatch, dim)
 
-        global_out = self.set2set(out, graph.batch)
+        # data.batch contains batchidxs for each atom in the graph
+        # e.g. batch consists of two molecules of natomsize 8 and 6 respectively
+        # then natomsbatch=8+6=14
+        # and data.batch = [0,0,0,0,0,0,0,0,1,1,1,1,1,1]
+        # data.batch.shape = (natomsbatch)
+        global_out = self.set2set(out, data.batch) # global_out has dim (num_mols_in_batch, 2*dim)
         global_out = F.dropout(global_out, training=do_dropout, p=self.dropout_rate)
-        per_mol_out = self.lin(global_out) # per mol scalar outputs
 
-        if hasattr(graph, 'nblocks'):
-            per_mol_out = per_mol_out * graph.nblocks.unsqueeze(1)
+        per_mol_out = self.lin1(global_out) # per_mol_out has shape (num_mols_in_batch, 1)
+        # i.e. a reward is computed for each molecule in the batch
 
         return per_mol_out
 
+# heavily inspired by code from https://github.com/recursionpharma/gflownet
 class Proxy:
     proxy_path = 'models/pretrained_proxy'
 
-    def __init__(self, device) -> None:
-        eargs = pickle.load(gzip.open(f'{self.proxy_path}/info.pkl.gz'))['args']
+    def __init__(self, device, dim=64, num_conv_steps=12):
+        # load model parameters from pretrained proxy
         params = pickle.load(gzip.open(f'{self.proxy_path}/best_params.pkl.gz'))
-        self.mdp = MoleculeMDP()
-        self.proxy = self.make_model(eargs)
-        for a,b in zip(self.proxy.parameters(), params):
-            a.data = torch.tensor(b, dtype='float64')
-        self.proxy.to(device)
 
-    def make_model(self, eargs):
-        if eargs.repr_type == 'atom_graph':
-            model = MPNNet_Atom(
-                num_feat = eargs.num_feat, 
-                num_vec = 0, 
-                dim = eargs.nemb,
-                out_per_mol = 1, 
-                out_per_stem = eargs.num_out_per_stem,
-                num_conv_steps = eargs.num_conv_steps, 
-                dropout_rate = eargs.dropout_rate)
+        self.device = device
+
+        self.nhid = 64
+        self.num_conv_steps = num_conv_steps
+
+        # make model
+        self.mpnn = self.make_model(params)
+
+    def make_model(self, params):
+        # number of features for each atom in the molecule
+        # one hot encoded
+        # the first 14 values will contain information about hybridization type, donor/acceptor type, etc.
+        # the extra 1 is used to create a stem mask
+        # the remaining 56 entries will denote which atom it is
+        num_feat = (14 + 1 + len(atomic_numbers))
+        mpnn = MPNNet_Atom(num_feat=num_feat, dim=self.nhid, num_conv_steps=self.num_conv_steps)
         
-        return model
+        # load only parameters that appears in the pretrained proxy
+        param_map = {
+            'lin0.weight': params[0],
+            'lin0.bias': params[1],
+            'conv.bias': params[3],
+            'conv.nn.0.weight': params[4],
+            'conv.nn.0.bias': params[5],
+            'conv.nn.2.weight': params[6],
+            'conv.nn.2.bias': params[7],
+            'conv.lin.weight': params[2],
+            'gru.weight_ih_l0': params[8],
+            'gru.weight_hh_l0': params[9],
+            'gru.bias_ih_l0': params[10],
+            'gru.bias_hh_l0': params[11],
+            'set2set.lstm.weight_ih_l0': params[16],
+            'set2set.lstm.weight_hh_l0': params[17],
+            'set2set.lstm.bias_ih_l0': params[18],
+            'set2set.lstm.bias_hh_l0': params[19],
+            'lin1.weight': params[20],
+            'lin1.bias': params[21],
+        }
+        # set values appropriately
+        for k, v in param_map.items():
+            mpnn.get_parameter(k).data = torch.tensor(v)
+        return mpnn
             
+    def compute_reward(self, mols):
+        # convert each molecule to an atomic graph
+        graphs = [mol.to_atom_graph() for mol in mols]
+        # check validity for each graph
+        is_valid = torch.tensor([i is not None for i in graphs]).bool()
+        # if every graph is None say the input batch is invalid
+        if not is_valid.any():
+            return torch.zeros((0, 1)), is_valid
         
+        # make into batch for all valid graphs
+        batch = Batch.from_data_list([i for i in graphs if i is not None])
+        batch.to(self.device)
 
-    def __call__(self, mol):
-        mol = mol.to_atom_graph()
-        return self.proxy(mol).item()
-        
+        # compute model predictions
+        preds = self.mpnn(batch).reshape((-1,)).data.cpu()
+        preds[preds.isnan()] = 0
+
+        # limit to values between 1e-4 and 100 (ensures positive rewards only)
+        preds = preds.clip(1e-4, 100).reshape((-1, 1))
+        return preds, is_valid
     
+    def __call__(self, mols):
+        preds, is_valid = self.compute_reward(mols)
+        assert is_valid.any(), "Invalid graphs"
+        return preds
+
+
+    
+    
+        
 if __name__ == "__main__":
-    molecule = MoleculeMDP()
+    molecule = BlockMolecule()
     device = torch.device("cpu")
 
-    proxy = Proxy(device=device)
+    block_list = [96, 36, 93]
 
-    print(proxy(molecule))
+    for block in block_list:
+        try:
+            molecule.add_block(block)
+        except:
+            break
+
+    molecule.jbonds = [[0, 1, 3, 6], [0, 2, 8, 0]]
+    molecule.stems = []
+
+    #proxy = Proxy(device=device)
+
+    """ for a in proxy.proxy.parameters():
+        if len(a.data.shape) == 1:
+            a.data = a.data.unsqueeze(0) """
+
+    proxy = Proxy(device)
+
+    # load data points
+    filename = "docked_mols.csv"
+    path = f"data/processed/{filename}"
+
+    df = pd.read_csv(path)
+
+    columns = df.columns
+    
+    # the remaining columns contains lists of numbers
+    # they are in string form however in the native dataframe
+    # should be converted to list type 
+    for name in columns[2:]:
+        df.loc[:,name] = df[name].apply(json.loads)
+
+    # lambda function to transform dockscore into same values as proxy rewards
+    mu, std = [-8.6, 1.1]
+    transform = lambda v: 4 - (v-mu) / std
+
+    avg_err = 0
+
+    for i in range(1000):
+        if i % 100 == 0:
+            print(i)
+        # build molecule
+        mol = BlockMolecule()
+
+        for block in df.blockidxs[i]:
+            mol.add_block(block)
+            
+        mol.jbonds = df.jbonds[i]
+        mol.stems = df.stems[i]
+
+        pred = proxy([mol])[0].item()
+        target = transform(df.dockscore[i])
+
+        avg_err += abs(pred - target)
+    
+    print(avg_err / 1000)
