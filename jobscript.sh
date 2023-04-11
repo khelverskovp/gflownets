7f93a871fbd782b6dd3831da69a4b4a8302b3554
marcusroberto02 <marcusrobertonielsen02@gmail.com>
2023-04-11 15:59:47 +0200

    updated bash script

--- jobscript.sh
+++ jobscript.sh
@@ -17,8 +17,8 @@
 # if you want to receive e-mail notifications on a non-default address
 ### -- Specify the output and error file. %J is the job-id --
 ### -- -o and -e mean append, -oo and -eo mean overwrite --
-#BSUB -o outputs/gpu_%J.out
-#BSUB -e outputs/gpu_%J.err
+#BSUB -o outputs_hpc/gpu_%J.out
+#BSUB -e outputs_hpc/gpu_%J.err
 # -- end of LSF options --
 
 nvidia-smi
@@ -29,6 +29,7 @@ module load cuda/11.6
 
 # activate virtual environment
 source /zhome/45/e/155478/gflowenv/bin/activate
+### source /zhome/7a/6/154675/gflowenv/bin/activate
 
 python3 src/models/train_model.py
 
