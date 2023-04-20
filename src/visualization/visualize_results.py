58af7be9254575ac208760726886bfc2785cd309
marcusroberto02 <marcusrobertonielsen02@gmail.com>
2023-04-20 12:09:38 +0200

    m

--- src/visualization/visualize_results.py
+++ src/visualization/visualize_results.py
@@ -14,18 +14,18 @@ import pandas as pd
 
 if __name__ == "__main__":
     # experiment_id
-    experiment_id = "experiment_1"
+    experiment_id = "experiment_3"
 
     # make leaf loss plot
     make_leaf_flow_loss_plot(experiment_id)
     
     make_rewards_plot(experiment_id)
 
-    make_reward_threshold_plot([5,6,7,7.5], experiment_id)
+    make_reward_threshold_plot([7,7.5,7.9,8], experiment_id)
 
     # make rewards 
     rewards = []
-    with gzip.open("results/experiment_1/rewards.pkl.gz") as fr:
+    with gzip.open(f"results/{experiment_id}/rewards.pkl.gz") as fr:
         try:
             while True:
                 rewards.extend(pickle.load(fr))
@@ -35,7 +35,46 @@ if __name__ == "__main__":
     print(np.max(rewards))
     print(len(rewards))
     print(np.mean(rewards))
+    
+    """ hp = None
 
+    with gzip.open(f"results/{experiment_id}/losses.pkl.gz") as fr:
+        try:
+            while True:
+                hp = pickle.load(fr)["hp"]
+                break
+        except EOFError:
+            pass
+
+
+    print(hp) """
+
+    trajs = []
+    count = 1
+
+    with gzip.open(f"results/{experiment_id}/trajectories.pkl.gz") as fr:
+        try:
+            while True:
+                trajs.extend(pickle.load(fr))
+        except EOFError:
+            pass
+    """ print(len(trajs))
+    d = {key: 0 for key in range(2,9)}
+    RT = 7.5
+    for i in range(len(rewards)):
+        if rewards[i] > RT:
+            mol = BlockMolecule()
+            for (bi, si) in trajs[i]:
+                mol.add_block(bi,si)
+            if len(trajs[i]) == 4:
+                mol.draw_mol_to_file(f"RT_{RT}/topmol_{count}",highlightBonds=True)
+                print(mol.get_smiles())
+                print(rewards[i])
+            count += 1
+            #print(mol.get_smiles())
+            d[len(trajs[i])] += 1
+            #print(len(trajs[i]))
+    print(d) """
     """ rewards = []
     with gzip.open("results/experiment_1/rewards.pkl.gz") as fr:
         try:
